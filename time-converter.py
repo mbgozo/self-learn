27a96ec65704fe3f03afa64a8f4201b65453eac5
MB Gozo <mbgozobuilds@gmail.com>
2025-11-04 21:45:11 +0000

    Validate User's entry

--- time-converter.py
+++ time-converter.py
@@ -7,12 +7,14 @@
 def convert_time():
     #Receives data from user
     minutes = int(input("Enter your total number of minutes for conversion: "))
-
+    #Validating users entry
+    if minutes < 0:
+        return "ERROR: Minutes cannot be a negative number. Please try again."
     #Here modulo is used to deduce the remaining minutes left from the user's input
     remaining_time = minutes % 60 
     #Here floor division is used to get the number of hours from the user's input
     hour = minutes // 60
-    result = f"The given {minutes} minutes is equivalent to {hour}hrs, {remaining_time}mins"
-    return result
+    return  f"The given {minutes} minutes is equivalent to {hour}hrs, {remaining_time}mins"
+
 
 print(convert_time())
