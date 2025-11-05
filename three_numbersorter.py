2bda9d5b757352ccb9dc671a0a745804a2ab8a4a
MB Gozo <mbgozobuilds@gmail.com>
2025-11-05 10:29:58 +0000

    Maximum logic implemented

--- three_numbersorter.py
+++ three_numbersorter.py
@@ -1,5 +1,5 @@
 # Write a Python function that accepts three distinct integer numbers as input.
-# Your function must use conditional logic to compare these three numbers and 
+# Your function must use conditional logic to compare these three numbers and
 # then correctly identify and print the largest number and the smallest number among the three.
 def three_numbersorter():
     a, b, c = input("Enter your three distinct integer").split()
@@ -8,14 +8,15 @@ def three_numbersorter():
     c = int(c)
     mins = a
     maxs = a
-    if maxs < b:
+    if a < b:
         maxs = b
-        if maxs < c:
-            maxs = c
-    elif mins > b:
+    if maxs < c:
+        maxs = c
+
+    if a > b:
         mins = b
-        if mins > c:
-            mins = c
+    if mins > c:
+        mins = c
     return f"The minimum number entered is {mins}, and the maximum is {maxs}"
 
 print(three_numbersorter())
