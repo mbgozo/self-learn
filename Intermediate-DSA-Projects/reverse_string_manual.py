0d5538d628d4c07b534cf8a78c5ae418e1313109
Michael B Gozo <mbgozobuilds@gmail.com>
2025-11-06 13:02:22 +0000

    Final Reverse with function

--- Intermediate-DSA-Projects/reverse_string_manual.py
+++ Intermediate-DSA-Projects/reverse_string_manual.py
@@ -8,13 +8,16 @@
 # Indexing: Accessing characters/elements by their index.
 # String Concatenation (or building a list of characters and joining them).
 
-user = input("Random text: ")
-list_user = list(user)
-print(list_user)
-reversed_list = []
+def reverse_string(): #function name
+    user = input("Random text: ") #Takes input from User
+    list_user = list(user) #Converts users input to list
+    #print(list_user)
+    reversed_list = [] # An empty list to be receiving iteration of the reversal
 
-for i in list_user:
-    reversed_list.insert(0,i)
+    for i in list_user: #for loop is used because the number of iterations to be completed is known(A definite data received)
+        reversed_list.insert(0,i) #This inserts the accessed data at the given index in the new list and all others coming through from the iteration pushes old data to the right for new entry till iteration ends
 
-final_reversed_string =  "".join(reversed_list)
-print(final_reversed_string)
+    final_reversed_string =  "".join(reversed_list) # Combines all list items into one string with no separator
+    return final_reversed_string
+
+print(reverse_string())
