2c0666b3e9bb9e674899a21d107f519ca4e65a01
Michael B Gozo <mbgozobuilds@gmail.com>
2025-11-17 13:23:03 +0000

    Finally understood match statement, writing 'match pattern' without a gaurd or if clause renders it as a wildcard

---
+++ gameplay.py
@@ -0,0 +1,95 @@
+# import random
+# secret_number = random.randint(1, 10)
+# guess = int(input("Enter a random number: "))
+# match secret_number:
+#      case secret_number if guess == secret_number:
+#          print("Congratulations, you guessed it!")
+#     #case secret_number if  guess > secret_number:
+#         #print("")
+# # the variable in the match statement == the case pattern
+# my_var = 'a'
+# x = 10
+
+# match my_var:
+#     case 'a' if x > 5:
+#         print('a')
+#     case 'b':
+#         print("b")
+
+
+# Challenge
+# Let’s create a fun game using Match Case statements! In this game, the user tries to guess a secret number chosen by the program.
+
+# Here’s what your program should do:
+
+# Import the random module: At the beginning of your code,
+#  add the line import random. This allows you to use functions from 
+# the random module, like generating random numbers.
+# Generate a secret number: Use random.randint(1, 10) to generate a 
+# random number between 1 and 10 (you can adjust the range if you want). 
+# Store this in a variable called secret_number.
+# Get user’s guess: Prompt the user to guess the number using input(). 
+# Convert the user’s input to an integer using int(). 
+# Store this in a variable called guess.
+# Match the guess:Use a Match Case statement to compare 
+# the user’s guess with the secret number:
+
+# If the guess is correct, 
+# display a message like “Congratulations, you guessed it!”
+# If the guess is too high, display a message like
+#  “Oops, your guess is a bit high. Try again!”
+# If the guess is too low, display a message like 
+# “Nope, your guess is a bit low. Give it another shot!”
+# Offer to play again: Ask the user if they want to play again using 
+# an if statement and user input.
+# Bonus Challenge:
+# Add a counter to keep track of the number of guesses the user takes.
+# Display the number of guesses it took the user to win the game.
+
+
+import random
+secret_number = random.randint(1, 10)
+print(secret_number)
+guess = int(input("Guess the number: "))
+
+
+match secret_number:
+    case secret_number if guess > secret_number:
+        print("Oops, your guess is a bit high. Try again!")
+    case secret_number if guess < secret_number:
+        print("Nope, your guess is a bit low. Give it another shot!")
+    case secret_number:
+        print('Congratulations, you guessed it!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+# secret_number = random.randint(1, 10)
+# guess = int(input("Enter a random number: "))
+
+# match guess:
+#     case n if n == secret_number:
+#         print("Congratulations, you guessed it right")
+#     case n if n > secret_number:
+#         print("Oops, your guess is a bit high. Try again")
+#     case n if n < secret_number:
+#         print("Nope, your guess is a bit low. Give it another shot!")
