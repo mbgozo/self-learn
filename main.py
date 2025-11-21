13163df40fdaef7d7ec64184363e49c73a01bda7
Michael B Gozo <mbgozobuilds@gmail.com>
2025-11-21 18:59:30 +0000

    Entry point/Script

---
+++ main.py
@@ -0,0 +1,13 @@
+import calculator
+
+sum1 = calculator.add(5,6)
+print(sum1)
+
+minus = calculator.subtract(8,3)
+print(minus)
+
+multiply = calculator.multiply(5, 2)
+print(multiply)
+
+divide = calculator.divide(4, 0)
+print(divide)
