f0748999134c857e8a85cad1ca416873811b7afb
Michael B Gozo <mbgozobuilds@gmail.com>
2025-11-21 16:18:00 +0000

    Set Operations to remove duplicate

--- random_nums.py
+++ random_nums.py
@@ -1,3 +1,12 @@
 import random
-rands = random.randint(1, 10)
-print(rands)
+# rands = random.randint(1, 10)
+# print(rands)
+numbers_list = []
+for i in range(10):
+    rands = random.randint(1, 10)
+    numbers_list.append(rands)
+    print(numbers_list)
+
+print(numbers_list)
+print(len(numbers_list))
+print(set(numbers_list))
